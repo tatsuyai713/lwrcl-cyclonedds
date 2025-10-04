2e4e1df6a61d5ff5fcbf3ac21a8e0750d973c7e2
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2025-10-04 18:44:22 +0900

    Move scripts folder

--- data_types/build_data_types_qnx.sh
+++ data_types/build_data_types_qnx.sh
@@ -2,7 +2,7 @@
 
 export PATH=/opt/cyclonedds/bin/:$PATH
 export LD_LIBRARY_PATH=/opt/cyclonedds/lib/:$LD_LIBRARY_PATH
-cd ../src/ros-data-types-cyclonedds/src
+cd ./src/ros-data-types-cyclonedds/src
 rm -rf ./build-qnx
 cmake  . -B build-qnx -DCMAKE_TOOLCHAIN_FILE=../../cmake/qnx_toolchain.cmake -DCMAKE_INSTALL_PREFIX=/opt/qnx/cyclonedds-libs -DCMAKE_PREFIX_PATH=/opt/qnx/cyclonedds/lib/cmake
 cmake --build build-qnx
